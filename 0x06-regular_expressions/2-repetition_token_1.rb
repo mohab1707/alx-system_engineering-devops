650048b4c81b74998a7b23d72e75f45e35bc37e2
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2024-02-02 15:07:52 -0800

    2

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+input_string = ARGV[0]
+
+pattern = /hbt+n/
+
+if input_string.match?(pattern)
+  puts input_string
+else
+  puts ""
+end
